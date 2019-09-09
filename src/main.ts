617dc780186486fdd915e3f0714bf21505019efa
Rob Parker <robert.parker@alertlogic.com>
2019-09-09 14:42:45 +0100

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Cesium from 'cesium';
 
 if (environment.production) {
   enableProdMode();
 }
 
+Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
